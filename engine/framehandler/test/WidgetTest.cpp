29d17d2e20609e458c02cc39a41a751fcfe637b7
THaarer <THaarer@luxoft.com>
2018-01-08 12:44:56 +0100

    Updated file preambles

--- engine/framehandler/test/WidgetTest.cpp
+++ engine/framehandler/test/WidgetTest.cpp
@@ -5,19 +5,19 @@
 **
 **   Copyright (C) 2017 Luxoft GmbH
 **
-**   This file is part of Safe Render.
+**   This file is part of Safe Renderer.
 **
-**   Safe Render is free software: you can redistribute it and/or
+**   Safe Renderer is free software: you can redistribute it and/or
 **   modify it under the terms of the GNU Lesser General Public
 **   License as published by the Free Software Foundation.
 **
-**   Safe Render is distributed in the hope that it will be useful,
+**   Safe Renderer is distributed in the hope that it will be useful,
 **   but WITHOUT ANY WARRANTY; without even the implied warranty of
 **   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 **   Lesser General Public License for more details.
 **
 **   You should have received a copy of the GNU Lesser General Public
-**   License along with Safe Render.  If not, see
+**   License along with Safe Renderer.  If not, see
 **   <http://www.gnu.org/licenses/>.
 **
 **   SPDX-License-Identifier: LGPL-3.0
